2c3d0531755bfb3d903d67e8bb9c5a5af73d0015
Dominic Faulring <domcodesjs@gmail.com>
2020-12-06 15:51:30 -0800

    fixing cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const path = require('path');
@@ -11,8 +11,23 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://handmedowns-client.herokuapp.com/'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  next();
+});
 app.use(helmet());
-// app.use(morgan('tiny'));
+app.use(morgan('tiny'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
