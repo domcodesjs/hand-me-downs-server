3d6ec02c7a340c2069295a2b4ec98475add43640
Dominic Faulring <domcodesjs@gmail.com>
2020-12-06 15:52:27 -0800

    modified cors

--- app.js
+++ app.js
@@ -10,22 +10,22 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://handmedowns-client.herokuapp.com/'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
-  );
-  next();
-});
+app.use(cors({ origin: 'https://handmedowns-client.herokuapp.com' }));
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     'Access-Control-Allow-Origin',
+//     'https://handmedowns-client.herokuapp.com/'
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-Requested-With,content-type'
+//   );
+//   next();
+// });
 app.use(helmet());
 app.use(morgan('tiny'));
 
