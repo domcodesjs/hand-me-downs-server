7a7bb72495051c2f486d984af60a44a5a9883ddc
Dominic Faulring <domcodesjs@gmail.com>
2020-12-06 15:55:23 -0800

    moved cors

--- app.js
+++ app.js
@@ -10,22 +10,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors({ origin: 'https://handmedowns-client.herokuapp.com' }));
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     'Access-Control-Allow-Origin',
-//     'https://handmedowns-client.herokuapp.com/'
-//   );
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-//   );
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'X-Requested-With,content-type'
-//   );
-//   next();
-// });
 app.use(helmet());
 app.use(morgan('tiny'));
 
@@ -35,6 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
+app.use(cors());
 app.use('/', require('./routes/index'));
 app.use('/orders', require('./routes/ordersRoutes'));
 app.use('/users', require('./routes/usersRoutes'));
