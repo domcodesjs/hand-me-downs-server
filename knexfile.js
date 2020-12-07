9cc3f2b12abfa162ed58503632176ede2dcabf82
Dominic Faulring <domcodesjs@gmail.com>
2020-12-07 07:41:42 -0800

    changed knexfile

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,16 @@ module.exports = {
       directory: __dirname + '/knex/seeds'
     }
   },
+  production: {
+    client: 'pg',
+    connection: DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/knex/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/knex/seeds'
+    }
+  },
   test: {
     client: 'pg',
     connection: TEST_DATABASE_URL,
