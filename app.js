41beb2be23e5f0d62ad552743e850c4d1b75b414
Dominic Faulring <domcodesjs@gmail.com>
2020-12-07 07:38:39 -0800

    deleted prev

--- app.js
+++ app.js
@@ -20,17 +20,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
-  );
-  next();
-});
-
 app.use('/', require('./routes/index'));
 app.use('/orders', require('./routes/ordersRoutes'));
 app.use('/users', require('./routes/usersRoutes'));
